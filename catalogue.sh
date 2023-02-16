b11f99c700da61f655803197505aa17f88318a89
murali km <muralikm874@gmail.com>
2023-02-17 03:59:42 +0530

    Roboshop catalogue Script

--- catalogue.sh
+++ catalogue.sh
@@ -3,7 +3,7 @@ LOG=/tmp/roboshop.log
 
 status_check() {
   if  [ $? -eq 0 ]; then
-      echo-e "\e[32m SUCCESS\e[0m"
+     echo -e "\e[32m SUCCESS\e[0m"
   else
 
      echo -e "\e[31m FAILURE\e[0m"
