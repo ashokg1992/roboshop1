35d5e7b88110ef536028c6d796b90cdf7e56f8ba
murali km <muralikm874@gmail.com>
2023-02-17 03:37:28 +0530

    Roboshop catalogue Script

--- catalogue.sh
+++ catalogue.sh
@@ -36,7 +36,7 @@ if  [ $? -eq 0 ]; then
     echo SUCCESS
 else
    echo FAILURE
-   echo "Refer log file for more information, LOG - ${LOG}"
+echo "Refer log file for more information, LOG - ${LOG}"
 exit
 fi
 
