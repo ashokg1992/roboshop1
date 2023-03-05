cfe459466c4e662d29dc4eb5a166b0cccfa6538b
km murali <muralikm874@gmail.com>
2023-03-05 17:39:09 +0530

    Roboshop redis Script

--- redis.sh
+++ redis.sh
@@ -9,12 +9,13 @@ dnf module enable redis:remi-6.2 -y &>>${LOG}
 status_check
 
 print_head "Install Redis"
-yum install redis -y &>>${LOG}
+yum install redis -y  &>>${LOG}
 status_check
 
 print_head "Update Redis Listen Address"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>${LOG}
 status_check
+
 
 print_head "Enable Redis"
 systemctl enable redis &>>${LOG}
