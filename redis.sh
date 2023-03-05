cd375e45a005a5e8181b0cf379b706e661a1e4a3
km murali <muralikm874@gmail.com>
2023-03-05 15:02:53 +0530

    Roboshop catalogue Script

--- redis.sh
+++ redis.sh
@@ -1,7 +1,7 @@
 source common.sh
 
 print_head "Setup Redis Repo"
-yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>${LOG}
+cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${LOG}
 status_check
 
 print_head "Enable Redis 6.2 dnf Module"
@@ -9,13 +9,12 @@ dnf module enable redis:remi-6.2 -y &>>${LOG}
 status_check
 
 print_head "Install Redis"
-yum install redis -y  &>>${LOG}
+yum install redis -y &>>${LOG}
 status_check
 
 print_head "Update Redis Listen Address"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>${LOG}
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf &>>${LOG}
 status_check
-
 
 print_head "Enable Redis"
 systemctl enable redis &>>${LOG}
