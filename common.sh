4eeeab7f2b8f7a7cc069ddf1d30d9f8f833e5228
km murali <muralikm874@gmail.com>
2023-03-21 15:58:07 +0530

    Roboshop common Script

--- common.sh
+++ common.sh
@@ -91,73 +91,25 @@ LOAD_SCHEMA() {
 }
 
 NODEJS() {
-    source common.sh
+  print_head "Configuring NodeJS Repos"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
+  status_check
 
-    print_head "Configuring Nodejs Repos"
-    curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
-    status_check
+  print_head "Install NodeJS"
+  yum install nodejs -y &>>${LOG}
+  status_check
 
-    print_head "Install Nodejs "
-    yum install nodejs -y &>>${LOG}
-    status_check
+  APP_PREREQ
 
-    print_head "Add Application User"
-    useradd roboshop &>>${LOG}
-    id roboshop &>>${LOG}
-    if [ $? -ne 0 ]; then
-      useradd roboshop &>>${LOG}
-    fi
-    status_check
+  print_head "Installing NodeJS Dependencies"
+  cd /app &>>${LOG}
+  npm install &>>${LOG}
+  status_check
 
-    mkdir -p /app &>>${LOG}
+  SYSTEMD_SETUP
 
-    print_head "Downaloading App content"
-    curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${LOG}
-    status_check
-
-    print_head "Cleanup Old Content"
-    rm -rf /app/* &>>${LOG}
-    status_check
-
-    print_head "Extracting App Content"
-    cd /app
-    unzip /tmp/${component}.zip &>>${LOG}
-    status_check
-
-    print_head "Installing Nodejs Dependencies"
-    cd /app &>>${LOG}
-    npm install &>>${LOG}
-    status_check
-
-    print_head "Configuring ${component} Service File"
-    cp ${script_location}/files/${component}.service /etc/systemd/system/${component}.service &>>${LOG}
-    status_check
-
-    print_head "Reload SystemD"
-    systemctl daemon-reload &>>${LOG}
-    status_check
-
-    print_head "Enable ${component} Service "
-    systemctl enable ${component} &>>${LOG}
-    status_check
-
-    print_head "Start ${component} Service "
-    systemctl start ${component} &>>${LOG}
-    status_check
-
-    print_head "Configuring Mongo Repo "
-    cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${LOG}
-    status_check
-
-    print_head "Install Mongo Clinet"
-    yum install mongodb-org -y &>>${LOG}
-    status_check
-
-    print_head "Load Schema"
-    mongo --host mongodb-dev.muralidevops.online </app/schema/${component}.js &>>${LOG}
-    status_check
-   }
-
+  LOAD_SCHEMA
+}
 MAVEN() {
 
   print_head "Install Maven"
