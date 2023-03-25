90e419b7a2078255d63f0e6ac6a2d54d49a477cd
ashok <ashok@gmil.com>
2023-03-25 11:39:37 +0530

    g

--- create-servers.sh
+++ create-servers.sh
@@ -46,6 +46,6 @@ fi
 
 
 for component in catalogue cart user shipping payment frontend mongodb mysql rabbitmq redis dispatch; do
-  COMPONENT="${component}-${env}"
+  COMPONENT="${component}=${env}"
   create_ec2
 done
